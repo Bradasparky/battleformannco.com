caadf48c1db98ffa2deb413cba872ccfcf32b596
Joel Fargas <joelfargas@revivn.com>
2025-03-13 20:13:54 -0400

    Add content module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/ui'
-  ]
-})
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/ui",
+  ],
+  devServer: {
+    port: 4000,
+  },
+});
