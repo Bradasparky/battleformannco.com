4ab5d8e36e156a13e2b7910f447f7582a6eb4954
Bradasparky <bradasparky12@yahoo.com>
2025-04-03 16:07:01 -0400

    Added rules page

--- components/DiscordServerInvite.vue
+++ components/DiscordServerInvite.vue
@@ -7,28 +7,28 @@
     <p class="discord-invite-header">
       Join the Discord server!
     </p>
-      <div class="discord-invite-bottom">
-        <img class="discord-invite-badge-image" src="https://cdn.discordapp.com/icons/1241512258450751539/1a947522b12f7d3d507d3bfb12e4527d.webp?size=160&quality=lossless">
-        <div class="discord-invite-server-info-top">
-          <span class="discord-invite-server-name">
-            Battle For Mann Co.
-          </span>
-          <!-- <span class="infoBadge_ae2544"><div class="guildIconV2Container__85d16"><div class="" tabindex="-1" role="button"><div class="flowerStarContainer__3e3b0 guildBadge__85d16" style="width: 18px; height: 18px;"><svg class="flowerStar__3e3b0" aria-hidden="true" role="img" width="16" height="16" viewBox="0 0 16 15.2"><path fill="var(--white-400)" fill-rule="evenodd" d="m16 7.6c0 .79-1.28 1.38-1.52 2.09s.44 2 0 2.59-1.84.35-2.46.8-.79 1.84-1.54 2.09-1.67-.8-2.47-.8-1.75 1-2.47.8-.92-1.64-1.54-2.09-2-.18-2.46-.8.23-1.84 0-2.59-1.54-1.3-1.54-2.09 1.28-1.38 1.52-2.09-.44-2 0-2.59 1.85-.35 2.48-.8.78-1.84 1.53-2.12 1.67.83 2.47.83 1.75-1 2.47-.8.91 1.64 1.53 2.09 2 .18 2.46.8-.23 1.84 0 2.59 1.54 1.3 1.54 2.09z"></path></svg><div class="childContainer__3e3b0"><svg aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" width="11" height="11" fill="none" viewBox="0 0 24 24"><path fill="var(--primary-500)" d="m2.4 8.4 8.38-6.46a2 2 0 0 1 2.44 0l8.39 6.45a2 2 0 0 1-.79 3.54l-.32.07-.82 8.2a2 2 0 0 1-1.99 1.8H16a1 1 0 0 1-1-1v-5a3 3 0 1 0-6 0v5a1 1 0 0 1-1 1H6.31a2 2 0 0 1-1.99-1.8L3.5 12l-.32-.07a2 2 0 0 1-.79-3.54Z" class=""></path></svg></div></div></div><span style="display: none;"></span></div></span> 
-          <div class="discord-invite-member-info">
-            <i style="height: 8px; width: 8px; background-color: rgb(67, 162, 90); border-radius: 4px; margin-right: 4px, ">
-              
-            </i>
-            <span></span>
-          </div>-->
-        </div>
-
-        <a href="https://discord.gg/EeGxbzmbAm" target="_blank">
-          <button class="discord-join-button">
-            Join
-          </button>
-        </a>
+    <div class="discord-invite-bottom">
+      <img class="discord-invite-badge-image" src="https://cdn.discordapp.com/icons/1241512258450751539/1a947522b12f7d3d507d3bfb12e4527d.webp?size=160&quality=lossless">
+      <div class="discord-invite-server-info-top">
+        <span class="discord-invite-server-name">
+          Battle For Mann Co.
+        </span>
+        <!-- <span class="infoBadge_ae2544"><div class="guildIconV2Container__85d16"><div class="" tabindex="-1" role="button"><div class="flowerStarContainer__3e3b0 guildBadge__85d16" style="width: 18px; height: 18px;"><svg class="flowerStar__3e3b0" aria-hidden="true" role="img" width="16" height="16" viewBox="0 0 16 15.2"><path fill="var(--white-400)" fill-rule="evenodd" d="m16 7.6c0 .79-1.28 1.38-1.52 2.09s.44 2 0 2.59-1.84.35-2.46.8-.79 1.84-1.54 2.09-1.67-.8-2.47-.8-1.75 1-2.47.8-.92-1.64-1.54-2.09-2-.18-2.46-.8.23-1.84 0-2.59-1.54-1.3-1.54-2.09 1.28-1.38 1.52-2.09-.44-2 0-2.59 1.85-.35 2.48-.8.78-1.84 1.53-2.12 1.67.83 2.47.83 1.75-1 2.47-.8.91 1.64 1.53 2.09 2 .18 2.46.8-.23 1.84 0 2.59 1.54 1.3 1.54 2.09z"></path></svg><div class="childContainer__3e3b0"><svg aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" width="11" height="11" fill="none" viewBox="0 0 24 24"><path fill="var(--primary-500)" d="m2.4 8.4 8.38-6.46a2 2 0 0 1 2.44 0l8.39 6.45a2 2 0 0 1-.79 3.54l-.32.07-.82 8.2a2 2 0 0 1-1.99 1.8H16a1 1 0 0 1-1-1v-5a3 3 0 1 0-6 0v5a1 1 0 0 1-1 1H6.31a2 2 0 0 1-1.99-1.8L3.5 12l-.32-.07a2 2 0 0 1-.79-3.54Z" class=""></path></svg></div></div></div><span style="display: none;"></span></div></span> 
+        <div class="discord-invite-member-info">
+          <i style="height: 8px; width: 8px; background-color: rgb(67, 162, 90); border-radius: 4px; margin-right: 4px, ">
+            
+          </i>
+          <span></span>
+        </div>-->
       </div>
+
+      <a href="https://discord.gg/EeGxbzmbAm" target="_blank">
+        <button class="discord-join-button">
+          Join
+        </button>
+      </a>
     </div>
+  </div>
 </template>
 
 <style scoped>
---
+++ pages/rules.vue
@@ -0,0 +1,136 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="rules-page" style="margin-bottom: 100px;">
+    <div class="rules-block">
+      <h2 class="tf2build rule-header">
+        Rules
+      </h2>
+  
+      <div class="rule-container">
+        <div>
+          <span class="rule-title">General Rules</span>
+          <ul class="rule-list">
+            <li class="rule-name">Spam</li>
+            <ul class="rule-desc">
+              <li>Do not repeatedly type out unintelligible gibberish</li>
+              <li>Do not repeatedly use binds which flood chat</li>
+              <li>Do not repeatedly use the @ command to ping players who you have the attention of</li>
+            </ul>
+          </ul>
+          <ul class="rule-list">
+            <li class="rule-name">Exploits</li>
+            <ul class="rule-desc">
+              <li>Do not exploit commands, gameplay features, or spots on maps</li>
+              <li>Exploits which may be unknown to admins should be reported on the discord</li>
+            </ul>
+          </ul>
+          <ul class="rule-list">
+            <li class="rule-name">Harassment</li>
+            <ul class="rule-desc">
+              <li>Do not make unwanted actions towards another player that makes them feel unwelcome, uncomfortable, or annoyed</li>
+              <li>The above applies whether it be done by an individual or a group of people</li>
+              <li>The above also relates to actions within gameplay that don't relate to the objective such as being told to stop after 
+                repeatedly hitting someone with a frying pan</li>
+            </ul>
+          </ul>
+          <ul class="rule-list">
+            <li class="rule-name">Hateful Content</li>
+            <ul class="rule-desc">
+              <li>Do not use hateful language in voice or text chat whether by your own hands, binds, or micspam</li>
+              <li>Do not socially engineer people into using hateful language</li>
+              <li>Do not try to get around the rules by implying or inserting context in order to give yourself an excuse
+                to use hateful language</li>
+              <li>Do not spray or equip items which depict hateful user generated content</li>
+            </ul>
+            <li class="rule-name">Illegal NSFW Content</li>
+            <ul class="rule-desc">
+              <li style="font-weight: bold">DO NOT</li>
+            </ul>
+          </ul>
+        </div>
+  
+        <div>
+          <span class="rule-title">VSH Gameplay Rules</span>
+          <ul class="rule-list">
+            <li class="rule-name">Friendlying is not allowed and is defined as:</li>
+            <ul class="rule-desc">
+              <li>Refusing to participate in the objective</li>
+              <li>Refusing to kill other players who aren't participating in the objective</li>
+              <li>Refusing to shoot/kill enemy players who are within effective range or dead to rights because they are
+                your friend</li>
+            </ul>
+          </ul>
+          <ul class="rule-list">
+            <li class="rule-name">Stalling is not allowed and is defined as:</li>
+            <ul class="rule-desc">
+              <li>Refusing to progress the game</li>
+              <li>Dragging out the game without doing the objective, especially when victory is trivial</li>
+            </ul>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.rules-block {
+  margin-top: 50px;
+  background-color: var(--tf2-dark);
+  border-radius: 8px;
+  border-color: var(--tf2-gray);
+  border-width: 2px;
+  width: 900px;
+  padding: 0px 24px 50px 24px;
+}
+
+.rule-container {
+  display: flex;
+  flex-direction: column;
+  row-gap: 20px;
+}
+
+.rule-header {
+  color: var(--tf2-beige);
+  margin-bottom: 20px;
+  padding-top: 10px;
+  font-size: 50px;
+}
+
+.rule-list,
+.rule-name,
+.rule-desc {
+  font-family: "Arial";
+  color: var(--tf2-beige);
+}
+
+.rule-title {
+  font-size: 24px;
+  font-family: "Arial";
+  font-weight: 600;
+  color: var(--tf2-beige);
+  margin-left: 12px;
+}
+
+.rule-list {
+  list-style-type: disc;
+  margin-left: 32px;
+}
+
+.rule-name {
+  font-size: 18px;
+  margin-top: 12px;
+  font-weight: 600;
+}
+
+.rule-desc {
+  font-size: 17px;
+  list-style-type: circle;
+  margin-left: 32px;
+  font-weight: 500;
+}
+</style>
