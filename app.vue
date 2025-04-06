bb382e21b4e1aceb1ea6520fa1613bcf68aedb4c
Joel Fargas <SapphSky@proton.me>
2025-04-06 17:28:04 -0400

    Add simple navigation header

--- app.vue
+++ app.vue
@@ -3,6 +3,7 @@
     <h1 class="tf2build primary">
       Battle For Mann Co.
     </h1>
+    <HeaderNav />
     <NuxtPage />
   </div>
 </template>
---
+++ components/HeaderNav.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <NuxtLink to="/" class="tf2build primary">Home</NuxtLink>
+    <NuxtLink to="/rules" class="tf2build primary">Rules</NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+div {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  gap: 2em;
+  margin: 0em 2em;
+}
+
+.router-link-active {
+  color: var(--tf2-beige);
+}
+</style>
