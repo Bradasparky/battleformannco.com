639a2da36ceb6c762779fc7b8c650e737e75483c
Bradasparky <bradasparky12@yahoo.com>
2025-05-22 23:45:19 -0400

    Added Space Below Home Page

--- components/HomePage.vue
+++ components/HomePage.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div>
+  <div class="home-page">
     <h2 class="tf2build secondary">
       VSH Server Features
     </h2>
@@ -21,9 +21,20 @@
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 
 h2 {
   margin: 1rem;
+}
+
+.home-page {
+  margin-bottom: 10rem;
+}
+
+@media (width <= $phone-res)
+{
+  .home-page {
+    margin-bottom: calc(10rem * var(--phone-res-mult));
+  }
 }
 </style>
