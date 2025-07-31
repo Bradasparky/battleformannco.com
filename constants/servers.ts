0b40cb240fa12c3f2311c2c0004b94b11f880cef
Bradasparky <bradasparky12@yahoo.com>
2025-07-31 16:42:08 -0400

    Rename server ids

--- constants/servers.ts
+++ constants/servers.ts
@@ -6,12 +6,12 @@ export interface Server {
 
 export const servers: Server[] = [
   {
-    id: "vsh1",
+    id: "vsh_1",
     name: "Battle For Mann Co. | Official VSH - #1",
     ip: "74.91.113.144:27015",
   },
   {
-    id: "vsh2",
+    id: "vsh_2",
     name: "Battle For Mann Co. | Official VSH - #2",
     ip: "74.91.113.144:27016",
   },
