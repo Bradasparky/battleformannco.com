a6e49238976153dc968f0556c30f8175cb7529ff
Bradasparky <bradasparky12@yahoo.com>
2025-09-12 23:08:29 -0400

    Updated connect addresses

--- constants/servers.ts
+++ constants/servers.ts
@@ -8,11 +8,11 @@ export const servers: Server[] = [
   {
     id: "vsh_1",
     name: "Battle For Mann Co. | Official VSH - #1",
-    ip: "193.239.237.36:27015",
+    ip: "74.91.122.168:27015",
   },
   {
     id: "vsh_2",
     name: "Battle For Mann Co. | Official VSH - #2",
-    ip: "193.239.237.36:27016",
+    ip: "74.91.122.168:27016",
   },
 ];
